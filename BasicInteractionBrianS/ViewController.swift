0736d2c42f724ca75775e51101b975e5b56d5f88
Brian Seaver <bseaver@d155.org>
2020-06-18 09:29:31 -0500

    Initial Commit

---
+++ BasicInteractionBrianS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicInteractionBrianS
+//
+//  Created by Brian Seaver on 6/18/20.
+//  Copyright © 2020 clc.seaver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
